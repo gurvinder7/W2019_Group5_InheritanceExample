0ac7d839e6f5c3d58ed827060ea6d9092f175c6b
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-14 18:58:17 -0500

    Student class created and properties added.

---
+++ W2019_Group5_InheritanceExample/Student.swift
@@ -0,0 +1,18 @@
+//
+//  Student.swift
+//  W2019_Group5_InheritanceExample
+//
+//  Created by Baturay Kayatürk on 14.02.2019.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+class Student {
+    
+    private var sid : Int
+    private var sname : String
+    
+    init() {
+        sid = 0
+        sname = String()
+    }
+}
