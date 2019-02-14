59fefe7c2886a763eda891888e9d7500fbefc9dd
MacStudent <macstudent@126-iMac26.local>
2019-02-14 18:39:22 -0500

    Initial Commit

---
+++ W2019_Group5_InheritanceExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  W2019_Group5_InheritanceExample
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
