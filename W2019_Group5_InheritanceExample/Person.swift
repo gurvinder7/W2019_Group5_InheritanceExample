daeea9870436b1a7bb279f3031f136b3c36abdcd
MacStudent <macstudent@126-iMac26.local>
2019-02-14 18:55:32 -0500

    person class created

---
+++ W2019_Group5_InheritanceExample/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  W2019_Group5_InheritanceExample
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Person
+{
+
+    
+}
