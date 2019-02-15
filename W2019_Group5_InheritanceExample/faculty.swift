74add7ada163c9ff7352df9f60d3e31c7046a3ff
MacStudent <macstudent@126-imac25.local>
2019-02-14 19:03:17 -0500

    new file added

---
+++ W2019_Group5_InheritanceExample/faculty.swift
@@ -0,0 +1,14 @@
+//
+//  faculty.swift
+//  W2019_Group5_InheritanceExample
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class faculty
+{
+    
+}
